f394dc492b54a99e77b8279e655308bca91639c7
Amila-Pathberiya <32818076+amila1986@users.noreply.github.com>
2019-12-01 17:25:58 +0530

    adding port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
         client: 'pg',
         connection: {
-          host : '127.0.0.1',
+          host : 'postgresql-rugged-16934',
           user : 'postgres',
           password : 'nevercan',
           database : 'smart_brain'
